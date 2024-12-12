c4230b75b0e14bee8c0c7d4c9e0fe307969ab4ba
juice_artur <asasin.lol10@gmail.com>
2024-12-12 23:34:01 +0200

    feat: added base shader code

--- HelloTriangleApplication.h
+++ HelloTriangleApplication.h
@@ -2,6 +2,7 @@
 #define GLFW_INCLUDE_VULKAN
 #include <GLFW/glfw3.h>
 
+#include <fstream>
 #include <iostream>
 #include <optional>
 #include <set>
@@ -24,6 +25,24 @@ struct SwapChainSupportDetails {
   std::vector<VkSurfaceFormatKHR> formats;
   std::vector<VkPresentModeKHR> presentModes;
 };
+
+static std::vector<char> readFile(const std::string& filename) {
+  std::ifstream file(filename, std::ios::ate | std::ios::binary);
+
+  if (!file.is_open()) {
+    throw std::runtime_error("failed to open file!");
+  }
+
+  size_t fileSize = (size_t)file.tellg();
+  std::vector<char> buffer(fileSize);
+
+  file.seekg(0);
+  file.read(buffer.data(), fileSize);
+
+  file.close();
+
+  return buffer;
+}
 
 class HelloTriangleApplication {
  public:
@@ -103,4 +122,5 @@ class HelloTriangleApplication {
   void createSwapChain();
   void createImageViews();
   void createGraphicsPipeline();
+  VkShaderModule createShaderModule(const std::vector<char>& code);
 };
